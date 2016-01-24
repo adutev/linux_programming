f26b3eaffdd0fc3ef2a9fce446e0eadb715dca98
adutev <adutev@gmail.com>
2016-01-24 19:03:35 +0200

    blah

--- 02.Homework/calculator.c
+++ 02.Homework/calculator.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(){
-	int i = 0;
-	int ch;
-	char line[32];
-
-	while((ch = getchar()) != EOF){
-		if(ch != '\n'){
-			printf("%c", ch);
-		} else {
-			break;
-		}
-	}
-	return 0;
+/*
+ * check if a digit
+ *      check if in a word
+ *          add current digit to current number
+ *      start new number
+ *      add current digit to current number
+ * check if
+ * 
+ */
+int main() {
+    char* line = NULL;
+    size_t size = 0;
+    int input_length = getline(&line, &size, stdin);
+    
+//    int in_a_word = 0;
+//    
+//    int i;
+//    for (i = 0; i < size; i++) {
+//        
+//        if(line[i] >= '0' && line[i] <= '9'){
+//            if(in_a_word) {
+//                printf("%c", line[i]);
+//            } else {
+//                in_a_word = 1;
+//                printf("\n%c", line[i]);
+//                
+//            }            
+//        } else {
+//            in_a_word = 0;
+//        }
+//    }
+    int num1;
+    char operator;
+    int result = sscanf(line, "%d %c", &num1, &operator);
+    while(result != NULL){
+        sscanf(line, "%d %c", &num1, &operator);
+        printf("%d %c\n", num1, operator);
+    }
+    return 0;
 }
